94d613db3ef6d54149ab5860cbb620c4e5546c6f
spica07 <spica07@gmail.com>
2016-08-21 21:45:10 +0900

    Added view

--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,6 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
-
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
-
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -38,3 +30,11 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
